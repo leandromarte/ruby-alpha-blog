dc5849220374bd1ef69bc3fda539c3687d0f41f0
Leandro <leandromarte@gmail.com>
2017-01-11 15:59:25 +0000

    Editing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,20 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.save
+      flash[:notice] = "Article successfully edited"
+      redirect_to article_path(@article)
+    else 
+      render "edit"
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
