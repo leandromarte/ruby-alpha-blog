1d15647e9d8aa2021a81d4c30bbcf787653a662a
Leandro <leandromarte@gmail.com>
2017-01-24 15:38:46 +0000

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true, 
+            uniqueness: {case_sensitive: false}, 
+            length: {minimum: 3, maximum: 45}
+            
+            REGEX_VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+            
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false}, 
+            length: {maximum: 100},
+            format: {with: REGEX_VALID_EMAIL}
 end
