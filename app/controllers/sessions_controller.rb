b5ece239f589e895a6ffd7af169f4dbdd00c885a
Leandro <leandromarte@gmail.com>
2017-02-03 15:13:47 +0000

    User's restrictions pt 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   def new
-  
+
   end
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     
-    if user && user.authenticate(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to user_path(user)      
     else
       flash.now[:danger] = "Login error" 
+      render 'new'
     end
   end
   
   def destroy
+    session[:user_id] = nil
     flash[:success] = "You have logged out"
     redirect_to root_path    
   end
