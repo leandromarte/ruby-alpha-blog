dfffa6bc38ab8a01ece5d654259ab440dadf13da
Leandro <leandromarte@gmail.com>
2017-02-06 14:57:53 +0000

    Users restrictions trhough controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show, :destroy, :update]
-  
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:new, :create, :edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -45,6 +46,13 @@ class ArticlesController < ApplicationController
   
   def show
 
+  end
+  
+  def require_same_user
+    if current_user == @article.user
+      flash[:danger] = "You cannot edit someone else's articles."
+      redirect_to root_path
+    end
   end
 
   private
