1cf19ea498808bd4e006354cf13efdb1bcc08d46
Leandro <leandromarte@gmail.com>
2017-02-15 15:48:52 +0000

    Require admin for manage categories pt2

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -7,6 +7,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     
@@ -19,6 +20,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
