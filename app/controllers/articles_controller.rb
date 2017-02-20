e21b379c2c3da98b2ef6efa96cf695ab0064510f
Leandro <leandromarte@gmail.com>
2017-02-20 15:41:55 +0000

    Associations between articles and categories through front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show, :destroy, :update]
   before_action :require_user, except: [:index, :show]
-  before_action :require_same_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
   end
   
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
 end
